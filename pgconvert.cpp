0a72eb4a0f95d882ff7fba98859c0cfc06466acc
Viktor Ostashevskyi <ostash@ostash.kiev.ua>
2013-03-21 12:35:13 +0100

    pgconvert: add command line parsing
    Allow to set mode, detail level and instruction dumping

--- pgconvert.cpp
+++ pgconvert.cpp
@@ -7,26 +7,95 @@
 #include <cstdlib>
 #include <cstring>
 
-int main(int argc, char** argv)
+
+struct Params
 {
-  if (argc < 3)
+  enum Detail
   {
-    std::cout << "Usage: " << program_invocation_short_name << " {flat|callgraph} filename.pgdata\n";
+    Object,
+    Symbol,
+    Source
+  };
+  Params()
+    : mode(Profile::CallGraph)
+    , detail(Symbol)
+    , dumpInstructions(false)
+    , inputFile(0)
+  {}
+  Profile::Mode mode;
+  Detail detail;
+  bool dumpInstructions;
+  const char* inputFile;
+};
+
+void parseArguments(Params& params, int argc, char* argv[])
+{
+  if (argc < 2)
+  {
+    std::cout << "Usage: " << program_invocation_short_name <<
+                 " [-m {flat|callgraph}] [-d {object|symbol|source}] [-i] filename.pgdata\n";
     exit(EXIT_SUCCESS);
   }
 
-  Profile::Mode mode;
-  if (strcmp(argv[1], "flat") == 0)
-    mode = Profile::Flat;
-  else if (strcmp(argv[1], "callgraph") == 0)
-    mode = Profile::CallGraph;
-  else
+  int nextArgType = 0;
+  for (argv++; *argv; argv++)
   {
-    std::cerr << "Invalid mode '" << argv[1] <<"'\n";
-    exit(EXIT_FAILURE);
+    char* argp = *argv;
+    switch(nextArgType)
+    {
+    case 0:
+      if (strcmp(argp, "-m") == 0)
+        nextArgType = 1;
+      else if (strcmp(argp, "-d") == 0)
+        nextArgType = 2;
+      else if (strcmp(argp, "-i") == 0)
+        params.dumpInstructions = true;
+      else
+        params.inputFile = argp;
+      break;
+    case 1:
+      // mode
+      nextArgType = 0;
+      if (strcmp(argp, "flat") == 0)
+        params.mode = Profile::Flat;
+      else if (strcmp(argp, "callgraph") == 0)
+        params.mode = Profile::CallGraph;
+      else
+      {
+        std::cerr << "Invalid mode '" << argp <<"'\n";
+        exit(EXIT_FAILURE);
+      }
+      break;
+    case 2:
+      // detail
+      nextArgType = 0;
+      if (strcmp(argp, "object") == 0)
+        params.detail = Params::Object;
+      else if (strcmp(argp, "symbol") == 0)
+        params.detail = Params::Symbol;
+      else if (strcmp(argp, "source") == 0)
+        params.detail = Params::Source;
+      else
+      {
+        std::cerr << "Invalid details level '" << argp <<"'\n";
+        exit(EXIT_FAILURE);
+      }
+    }
   }
 
-  std::fstream input(argv[2], std::ios_base::in);
+  if (!params.inputFile)
+  {
+    std::cerr << "Not input file given\n";
+    exit(EXIT_FAILURE);
+  }
+}
+
+int main(int argc, char** argv)
+{
+  Params params;
+  parseArguments(params, argc, argv);
+
+  std::fstream input(params.inputFile, std::ios_base::in);
   if (!input)
   {
     std::cerr << "Error reading input file " << argv[1] << '\n';
@@ -34,7 +103,7 @@ int main(int argc, char** argv)
   }
 
   Profile profile;
-  profile.load(input, mode);
+  profile.load(input, params.mode);
   input.close();
 
   for (MemoryObjectStorage::const_iterator objIt = profile.memoryObjects().begin();
@@ -49,7 +118,7 @@ int main(int argc, char** argv)
     }
   }
 
-  if (mode == Profile::CallGraph)
+  if (params.mode == Profile::CallGraph)
     profile.fixupBranches();
 
   return 0;
